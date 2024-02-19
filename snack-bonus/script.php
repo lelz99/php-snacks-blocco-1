146fa38a364206a348a8c8174a6854d039092fe9
lelz99 <emanuelesimonetti99@gmail.com>
2024-02-19 17:08:41 +0100

    snack bonus svolto

---
+++ snack-bonus/script.php
@@ -0,0 +1,19 @@
+<?php
+
+$paragraph = $_POST['paragraph'];
+$paragraph = explode('.', $paragraph);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($paragraph as $paragraphs) : ?>
+    <p><?= $paragraphs ?></p>
+    <?php endforeach?>
+</body>
+</html>
